5625b36838dd0dbffbca394325f3ee8e25d7fae7
thisisaaronland <devnull@localhost>
2020-02-05 12:25:58 -0800

    initial commit, untested

---
+++ geojson.go
@@ -0,0 +1,93 @@
+package geojson
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"strings"
+)
+
+const NS_GEOJSON string = "https://purl.org/geojson/vocab#"
+
+func DefaultGeoJSONLDContext() map[string]interface{} {
+
+	bbox := map[string]string{
+		"@container": "@list",
+		"@id":        "geojson:bbox",
+	}
+
+	coords := map[string]string{
+		"@container": "@list",
+		"@id":        "geojson:coordinates",
+	}
+
+	features := map[string]string{
+		"@container": "@set",
+		"@id":        "geojson:features",
+	}
+
+	ctx := map[string]interface{}{
+		"geojson":            NS_GEOJSON,
+		"Feature":            "geojson:Feature",
+		"FeatureCollection":  "geojson:FeatureCollection",
+		"GeometryCollection": "geojson:GeometryCollection",
+		"LineString":         "geojson:LineString",
+		"MultiLineString":    "geojson:MultiLineString",
+		"MultiPoint":         "geojson:MultiPoint",
+		"MultiPolygon":       "geojson:MultiPolygon",
+		"Point":              "geojson:Point",
+		"Polygon":            "geojson:Polygon",
+		"bbox":               bbox,
+		"coordinates":        coords,
+		"features":           features,
+		"geometry":           "geojson:geometry",
+		"id":                 "@id",
+		"properties":         "geojson:properties",
+		"type":               "@type",
+	}
+
+	return ctx
+}
+
+func AsGeoJSONLD(ctx context.Context, body []byte) ([]byte, error) {
+
+	geojson_ctx := DefaultGeoJSONLDContext()
+
+	props_rsp := gjson.GetBytes(body, "properties")
+
+	if !props_rsp.Exists() {
+		return nil, errors.New("Missing properties")
+	}
+
+	for k, v := range props_rsp.Map() {
+
+		parts := strings.Split(k, ":")
+
+		var k_fq string
+
+		if len(parts) == 2 {
+
+			ns := parts[0]
+			pred := parts[1]
+
+			// sudo make this dynamic / a callback / equivalent
+
+			k_fq = fmt.Sprintf("https://github.com/whosonfirst/whosonfirst-properties/tree/master/properties/%s#%s", ns, pred)
+		} else {
+
+			k_fq = fmt.Sprintf("http://whosonfirst.org/#%s", k)
+		}
+
+		geojson_ctx[k_fq] = v
+	}
+
+	body, err := sjson.SetBytes(body, "@context", geojson_ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/whosonfirst/go-geojson-ld
+
+go 1.12
+
+require (
+	github.com/tidwall/gjson v1.4.0
+	github.com/tidwall/sjson v1.0.4
+)
